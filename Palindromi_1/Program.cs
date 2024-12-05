528db50781e2b7b5aab165a614beb1586cd9b509
Radoinov <radoinov08@gmail.com>
2024-12-05 21:38:03 +0200

    Dr

---
+++ Palindromi_1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Palindromi_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi izrechenie: ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split();
+
+            Stack<string> stack = new Stack<string>();
+            
+            foreach (string word in words)
+            {
+                stack.Push(word);
+            }
+            
+            Console.WriteLine($"Broj dumi: {stack.Count}");
+            
+            Console.Write("Obraten red: ");
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
+
+
+
---
+++ Palindromi_2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Palindromi_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dai duma: ");
+            string word = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                stack.Push(word[i]);
+            }
+
+            bool Palindrom = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != stack.Pop())
+                {
+                    Palindrom = false;
+                    break;
+                }
+            }
+            if (Palindrom == true)
+            {
+                Console.WriteLine("dumata e palindrom");
+            }
+            else
+            {
+                Console.WriteLine("dumata ne e palindrom");
+            }
+        }
+    }
+}
