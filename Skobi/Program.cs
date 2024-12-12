57f50922e2bd11de5d07835e33d0a4eff8049cfa
Radoinov <radoinov08@gmail.com>
2024-12-12 19:35:59 +0200

    Dr

---
+++ Skobi/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Skobi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Daj skobi: ");
+            string brackets = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            bool isBalanced = true;
+
+            foreach (char item in brackets)
+            {
+                if (item == '(')
+                {
+                    stack.Push(item);
+                }
+                else if (item == ')')
+                {
+                    if (stack.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                    stack.Pop();
+                }
+            }
+
+            if (isBalanced && stack.Count == 0)
+            {
+                Console.WriteLine("Balansirano");
+            }
+            else
+            {
+                Console.WriteLine("Ne e balansirano");
+            }
+        }
+    }
+}
